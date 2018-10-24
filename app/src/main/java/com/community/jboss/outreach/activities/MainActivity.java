2d5217c1084ee856108ab90d023c5334a6755624
Mickle_Liutov <20002misha@gmail.com>
2018-10-24 20:41:48 +0300

    Add swipe to refresh

--- app/src/main/java/com/community/jboss/outreach/activities/MainActivity.java
+++ app/src/main/java/com/community/jboss/outreach/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.community.jboss.outreach.activities;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +23,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_list);
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh);
 
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                requestData();
+            }
+        });
     }
 
     private void requestData(){
         new ApiHandler(this).execute();
     }
     public void receiveData(String[][] data){
+        swipeRefreshLayout.setRefreshing(false);
         if(data!=null) {
             mAdapter = new RepositListRecyclerAdapter(data);
             mRecyclerView.setAdapter(mAdapter);
